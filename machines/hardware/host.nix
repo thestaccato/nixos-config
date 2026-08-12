c00153dad839f14c96a040709d6c746eff8df439
Yash Sharma <296458454+thestaccato@users.noreply.github.com>
2026-08-12 13:12:23 +0530

    setup hardware config for host

--- machines/hardware/host.nix
+++ machines/hardware/host.nix
@@ -5,6 +5,34 @@
 # file with the output of `nixos-generate-config` run on this machine. The
 # example below assumes a disk layout identical to the one used for our VMs:
 # a "nixos" root partition (ext4) and a "boot" EFI partition (vfat).
+
 { config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-label/nixos";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-label/boot";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
+
+  swapDevices = [ ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
 
 
